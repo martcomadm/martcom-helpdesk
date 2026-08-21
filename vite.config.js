682d6e5c9dfe232fbc54176207c56621fb6bdc92
martcomadm <martcomadm@gmail.com>
2026-08-21 13:07:52 -0600

    Allow EasyPanel host in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['martcom-validaciones-helpdesk-web.mchc0z.easypanel.host'],
+  },
+});
