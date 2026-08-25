4d3447c5cd83aceaff6dda5272a76c23ef0106f2
martcomadm <martcomadm@gmail.com>
2026-08-25 12:40:02 -0600

    feat: show reopen history on ticket detail

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ import MyTickets from './pages/MyTickets';
 import TicketDetail from './pages/TicketDetail';
 import SupportPanel from './pages/SupportPanel';
 import NotificationBell from './components/NotificationBell';
+import ReopenHistoryPanel from './components/ReopenHistoryPanel';
 
 function ProtectedRoute({ children }) {
   if (!pb.authStore.isValid) return <Navigate to="/login" replace />;
   return <><NotificationBell />{children}</>;
+}
+
+function TicketDetailRoute() {
+  return <ProtectedRoute><TicketDetail /><ReopenHistoryPanel /></ProtectedRoute>;
 }
 
 export default function App() {
@@ -20,7 +25,7 @@ export default function App() {
       <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       <Route path="/tickets/new" element={<ProtectedRoute><CreateTicket /></ProtectedRoute>} />
       <Route path="/tickets/mine" element={<ProtectedRoute><MyTickets /></ProtectedRoute>} />
-      <Route path="/tickets/:id" element={<ProtectedRoute><TicketDetail /></ProtectedRoute>} />
+      <Route path="/tickets/:id" element={<TicketDetailRoute />} />
       <Route path="/support" element={<ProtectedRoute><SupportPanel /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
