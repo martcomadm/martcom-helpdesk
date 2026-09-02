9f578118be0042ce67c4390bccbe8bd5c89f0a6c
martcomadm <martcomadm@gmail.com>
2026-09-02 10:41:25 -0600

    feat: load mobile responsive styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './sla-detail.css';
+import './mobile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
